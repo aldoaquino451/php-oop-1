15e7fce43f30a2b2a16f4cdc97ea8dc629aeb39c
aldoaquino451 <aldoaquino2909@gmail.com>
2023-11-06 17:19:58 +0100

    [init] index

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__ . "./Model/Movie.php";
+require_once __DIR__ . "./db/db.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- bootstrap  -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css'
+    integrity='sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A=='
+    crossorigin='anonymous' />
+
+  <!-- titolo  -->
+  <title>OOP</title>
+
+</head>
+
+<body>
+
+  <section class="container" id="movies">
+    <h1 class="my-3">Movies</h1>
+
+    <div>
+      Cards
+    </div>
+
+  </section>
+
+</body>
+
+</html>
